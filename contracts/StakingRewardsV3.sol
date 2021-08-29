94ece66d6939c98367049643d1b636fcc35d60d1
Andre Cronje <andre.cronje@yearn.finance>
2021-08-29 11:14:43 +0200

    Optimized array re-index (thank you PeckShield)
    Signed-off-by: Andre Cronje <andre.cronje@yearn.finance>

--- contracts/StakingRewardsV3.sol
+++ contracts/StakingRewardsV3.sol
@@ -12,13 +12,13 @@ library Math {
 
 library PoolAddress {
     bytes32 internal constant POOL_INIT_CODE_HASH = 0xe34f199b19b2b4f47f68442619d555527d244f78a3297ea89325f843f87b8b54;
-    
+
     struct PoolKey {
         address token0;
         address token1;
         uint24 fee;
     }
-    
+
     function computeAddress(address factory, PoolKey memory key) internal pure returns (address pool) {
         require(key.token0 < key.token1);
         pool = address(
@@ -60,7 +60,7 @@ interface PositionManagerV3 {
             uint128 tokensOwed1
         );
     function safeTransferFrom(address from, address to, uint tokenId) external;
-    
+
     function ownerOf(uint tokenId) external view returns (address);
     function transferFrom(address from, address to, uint tokenId) external;
 }
@@ -78,46 +78,46 @@ interface UniV3 {
 }
 
 contract StakingRewardsV3 {
-    
+
     address immutable public reward;
     address immutable public pool;
-    
+
     address constant factory = 0x1F98431c8aD98523631AE4a59f267346ea31F984;
     PositionManagerV3 constant nftManager = PositionManagerV3(0xC36442b4a4522E871399CD717aBDD847Ab11FE88);
     uint constant DURATION = 7 days;
-    
+
     uint rewardRate;
     uint periodFinish;
     uint lastUpdateTime;
     uint rewardPerSecondStored;
-    
+
     mapping(uint => uint) public tokenRewardPerSecondPaid;
     mapping(uint => uint) public rewards;
-    
+
     struct time {
         uint32 timestamp;
         uint160 secondsPerLiquidityInside;
     }
-    
+
     mapping(uint => time) public elapsed;
     mapping(uint => address) public owners;
     mapping(address => uint[]) public tokenIds;
     mapping(uint => uint) public liquidityOf;
-    
+
     event RewardPaid(address indexed sender, uint tokenId, uint reward);
     event RewardAdded(address indexed sender, uint reward);
     event Deposit(address indexed sender, uint tokenId, uint liquidity);
     event Withdraw(address indexed sender, uint tokenId, uint liquidity);
-    
+
     constructor(address _reward, address _pool) {
         reward = _reward;
         pool = _pool;
     }
-    
+
     function getTokenIds(address owner) external view returns (uint[] memory) {
         return tokenIds[owner];
     }
-    
+
     function lastTimeRewardApplicable() public view returns (uint) {
         return Math.min(block.timestamp, periodFinish);
     }
@@ -128,7 +128,7 @@ contract StakingRewardsV3 {
 
     function earned(uint tokenId) public view returns (uint claimable, uint160 secondsPerLiquidityInside) {
         uint _reward = rewardPerSecond() - tokenRewardPerSecondPaid[tokenId];
-            
+
         time memory _elapsed = elapsed[tokenId];
         uint secondsInside;
         (secondsPerLiquidityInside, secondsInside) = _getSecondsInside(tokenId);
@@ -151,19 +151,19 @@ contract StakingRewardsV3 {
 
         require(pool == _pool);
         require(_liquidity > 0);
-        
+
         liquidityOf[tokenId] = _liquidity;
-        
+
         elapsed[tokenId] = time(uint32(lastTimeRewardApplicable()), _secondsPerLiquidityInside);
-        
+
         owners[tokenId] = msg.sender;
         tokenIds[msg.sender].push(tokenId);
-        
+
         nftManager.transferFrom(msg.sender, address(this), tokenId);
-        
+
         emit Deposit(msg.sender, tokenId, _liquidity);
     }
-    
+
     function _findIndex(uint[] memory array, uint element) internal pure returns (uint i) {
         for (i = 0; i < array.length; i++) {
             if (array[i] == element) {
@@ -171,14 +171,15 @@ contract StakingRewardsV3 {
             }
         }
     }
-    
-    function _remove(uint[] storage array, uint element) internal {
-        uint index = _findIndex(array, element);
-        if (index >= array.length) return;
 
-        for (uint i = index; i < array.length-1; i++){
-            array[i] = array[i+1];
+    function _remove(uint[] storage array, uint element) internal {
+        uint _index = _findIndex(array, element);
+        uint _length = array.length;
+        if (_index >= _length) return;
+        if (_index < _length-1) {
+            array[_index] = array[_length-1];
         }
+
         array.pop();
     }
 
@@ -189,10 +190,10 @@ contract StakingRewardsV3 {
         owners[tokenId] = address(0);
         _remove(tokenIds[msg.sender], tokenId);
         nftManager.safeTransferFrom(address(this), msg.sender, tokenId);
-        
-        emit Deposit(msg.sender, tokenId, _liquidity);
+
+        emit Withdraw(msg.sender, tokenId, _liquidity);
     }
-    
+
     function getRewards() external {
         uint[] memory _tokens = tokenIds[msg.sender];
         for (uint i = 0; i < _tokens.length; i++) {
@@ -205,11 +206,11 @@ contract StakingRewardsV3 {
         if (_reward > 0) {
             rewards[tokenId] = 0;
             _safeTransfer(reward, _getRecipient(tokenId), _reward);
-            
+
             emit RewardPaid(msg.sender, tokenId, _reward);
         }
     }
-    
+
     function _getRecipient(uint tokenId) internal view returns (address) {
         if (owners[tokenId] != address(0)) {
             return owners[tokenId];
@@ -217,7 +218,7 @@ contract StakingRewardsV3 {
             return nftManager.ownerOf(tokenId);
         }
     }
-    
+
     function exit() external {
         uint[] memory _tokens = tokenIds[msg.sender];
         for (uint i = 0; i < _tokens.length; i++) {
@@ -232,7 +233,7 @@ contract StakingRewardsV3 {
         withdraw(tokenId);
         getReward(tokenId);
     }
-    
+
     function notify(uint amount) external update(0) {
         _safeTransferFrom(reward, msg.sender, address(this), amount);
         if (block.timestamp >= periodFinish) {
@@ -242,10 +243,10 @@ contract StakingRewardsV3 {
             uint _leftover = _remaining * rewardRate;
             rewardRate = (amount + _leftover) / DURATION;
         }
-        
+
         lastUpdateTime = block.timestamp;
         periodFinish = block.timestamp + DURATION;
-        
+
         emit RewardAdded(msg.sender, amount);
     }
 
@@ -256,25 +257,25 @@ contract StakingRewardsV3 {
             (uint _reward, uint160 _secondsPerLiquidityInside) = earned(tokenId);
             tokenRewardPerSecondPaid[tokenId] = rewardPerSecondStored;
             rewards[tokenId] = _reward;
-            
+
             if (elapsed[tokenId].timestamp < lastUpdateTime) {
                 elapsed[tokenId] = time(uint32(lastUpdateTime), _secondsPerLiquidityInside);
             }
         }
         _;
     }
-    
+
     function _getSecondsInside(uint256 tokenId) internal view returns (uint160 secondsPerLiquidityInside, uint secondsInside) {
         (,,,,,int24 tickLower,int24 tickUpper,,,,,) = nftManager.positions(tokenId);
         (,secondsPerLiquidityInside,secondsInside) = UniV3(pool).snapshotCumulativesInside(tickLower, tickUpper);
     }
-    
+
     function _safeTransfer(address token, address to, uint256 value) internal {
         (bool success, bytes memory data) =
             token.call(abi.encodeWithSelector(erc20.transfer.selector, to, value));
         require(success && (data.length == 0 || abi.decode(data, (bool))));
     }
-    
+
     function _safeTransferFrom(address token, address from, address to, uint256 value) internal {
         (bool success, bytes memory data) =
             token.call(abi.encodeWithSelector(erc20.transferFrom.selector, from, to, value));
